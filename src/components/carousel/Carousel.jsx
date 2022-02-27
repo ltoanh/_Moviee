7eef8cb8e3e28f2141f4658c08b2f19eac2a83f9
whiwf <leetusoanh@gmail.com>
2022-02-27 09:41:08 +0700

    Update responsive

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,16 +1,40 @@
 import Card from 'components/card/Card';
-
+import 'assets/styles/sass-utils/_variables.scss';
 import React from 'react';
-
 // swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Scrollbar } from 'swiper';
+// install Swiper modules
+SwiperCore.use([Scrollbar]);
+
+const swiperBreakpoints = {
+	320: {
+		width: 320,
+		slidesPerView: 1,
+	},
+	560: {
+		with: 560,
+		slidesPerView: 2,
+	},
+	768: {
+		width: 768,
+		slidesPerView: 3,
+	},
+	1024: {
+		width: 1024,
+		slidesPerView: 4,
+	},
+};
 
 function Carousel({ listData, category }) {
 	return (
 		<div className="carousel-section__content">
 			<Swiper
 				grabCursor={true} // hinh ban tay when scroll
-				slidesPerView={4}
+				breakpoints={swiperBreakpoints}
+				scrollbar={{
+					hide: false,
+				}}
 				spaceBetween={20}
 			>
 				{listData.map((item) => (
